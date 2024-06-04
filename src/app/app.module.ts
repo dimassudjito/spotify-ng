5ec8ea3b6493763a27ec25426d1b77ef119425d8
Dimas Sudjito <almuhtad@ualberta.ca>
2024-06-03 20:39:17 -0400

    add core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
+import { TabComponent } from './core/tab/tab.component';
+import { LibraryComponent } from './core/library/library.component';
+import { PlayerComponent } from './core/player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    TabComponent,
+    LibraryComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
